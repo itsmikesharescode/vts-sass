9b5c62bdb37bb0828aa800f7dcb0865f58d9db45
Mike John Eviota <eviotamikejohnb@gmail.com>
2025-03-16 20:27:53 +0800

    add table

---
+++ src/routes/admin/positions/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts" module>
+  import CTable from '$lib/components/general/custom-table/ctable.svelte';
+  import { columns } from './components/table/columns.js';
+</script>
+
+<script lang="ts">
+  const data = [
+    { id: crypto.randomUUID(), position_name: 'Position 1', position_info: 'Position 1 info' },
+    { id: crypto.randomUUID(), position_name: 'Position 2', position_info: 'Position 2 info' }
+  ];
+</script>
+
+<main>
+  <CTable {columns} {data} />
+</main>
---
+++ src/routes/admin/positions/components/table/components/row-actions.svelte
@@ -0,0 +1,30 @@
+<script lang="ts" module>
+  type TData = unknown;
+</script>
+
+<script lang="ts" generics="TData">
+  import Ellipsis from 'lucide-svelte/icons/ellipsis';
+  import type { Row } from '@tanstack/table-core';
+  import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+  import Button from '$lib/components/ui/button/button.svelte';
+  import type { PositionTable } from '../schema.js';
+
+  let { row }: { row: Row<PositionTable> } = $props();
+</script>
+
+<div class="flex justify-end">
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger>
+      {#snippet child({ props })}
+        <Button {...props} variant="ghost" class="flex h-8 w-8 p-0 data-[state=open]:bg-muted">
+          <Ellipsis />
+          <span class="sr-only">Open Menu</span>
+        </Button>
+      {/snippet}
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content class="w-[160px]" align="end">
+      <DropdownMenu.Item>Edit</DropdownMenu.Item>
+      <DropdownMenu.Item>Delete</DropdownMenu.Item>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
+</div>
